0eab50befa498181f76d2bd0f06461caa9d00fd4
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-30 13:39:03 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-30 13:39:03

--- modules/welcome.py
+++ modules/welcome.py
@@ -2,4 +2,4 @@ from telegram_bot_panel import *
 @bot.on(events.NewMessage(pattern="/start"))
 async def start(event):
     pass
-#	await event.reply("Halo Tokomardvers!")
+#	await event.reply("Selamat datang di Bot Tokomard! ketik /menu untuk mulai.")
