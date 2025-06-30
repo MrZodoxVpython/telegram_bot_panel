8f1a11cc4353032b5be54f29ba2ac4deb5fa4941
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-30 22:17:39 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-30 22:17:39

--- modules/trojan/create_trojan.py
+++ modules/trojan/create_trojan.py
@@ -52,8 +52,7 @@ async def create_trojan(event):
                 if f"#{tag}" in stripped and not inserted:
                     # Tambahkan komentar dan baris JSON dengan format yang benar
                     new_lines.append(comment)
-                    new_lines.append(f"},")
-                    new_lines.append(f"{entry}")
+                    new_lines.append(f'}},{entry[1:]}')
                     inserted = True
 
             if inserted:
