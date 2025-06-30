014ffbef970b5be4f0fc44e5da0902336babdd51
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-30 13:05:38 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-30 13:05:38

--- __main__.py
+++ __main__.py
@@ -3,6 +3,6 @@ from xolpanel import *
 from importlib import import_module
 from telegram_bot_panel.modules import ALL_MODULES
 for module_name in ALL_MODULES:
-        imported_module = import_module("xolpanel.modules." + module_name)
+        imported_module = import_module("telegram_bot_panel.modules." + module_name)
 bot.run_until_disconnected()
 
