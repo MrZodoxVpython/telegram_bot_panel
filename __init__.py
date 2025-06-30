7282f0084ac696f73d9bbba2ffd51fbf684ba37a
MrZodoxVpython <mrzodoxvpython@gmail.com>
2025-06-30 13:05:18 +0700

    Auto commit developing progress By Benjamin.Wickman : 2025-06-30 13:05:18

--- __init__.py
+++ __init__.py
@@ -3,23 +3,23 @@ import datetime as DT
 from telethon import *
 import requests,time,os,subprocess,re,sqlite3,sys,random
 import logging
-from xolpanel.text2png import text2png
+from telegram_bot_panel.text2png import text2png
 logging.basicConfig(level=logging.INFO)
 uptime = DT.datetime.now()
 
-exec(open("xolpanel/var.txt","r").read())
+exec(open("telegram_bot_panel/var.txt","r").read())
 bot = TelegramClient("XolPanel","6","eb06d4abfb49dc3eeb1aeb98ae0f581e").start(bot_token=BOT_TOKEN)
 try:
-	open("xolpanel/database.db")
+	open("telegram_bot_panel/database.db")
 except:
-	x = sqlite3.connect("xolpanel/database.db")
+	x = sqlite3.connect("telegram_bot_panel/database.db")
 	c = x.cursor()
 	c.execute("CREATE TABLE admin (user_id)")
 	c.execute("INSERT INTO admin (user_id) VALUES (?)",(ADMIN,))
 	x.commit()
 
 def get_db():
-	x = sqlite3.connect("xolpanel/database.db")
+	x = sqlite3.connect("telegram_bot_panel/database.db")
 	x.row_factory = sqlite3.Row
 	return x
 
